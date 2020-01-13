2d3745cc48da2829c401db89daf272dc51981efa
algot <algot@ua.fm>
2020-01-13 15:58:47 +0200

    Fix filename generator

--- loader.py
+++ loader.py
@@ -43,8 +43,8 @@ def get_list_of_gpx_files():
     return gpx_tracks
 
 
-# hostname = 'https://master.apis.dev.openstreetmap.org'
-hostname = 'https://api.openstreetmap.org'
+hostname = 'https://master.apis.dev.openstreetmap.org'
+# hostname = 'https://api.openstreetmap.org'
 
 current_session = get_session()
 
@@ -52,7 +52,7 @@ files = get_list_of_gpx_files()
 
 for file in files:
     print('Loading track: ' + file)
-    current_loaded_track_id = post_gpx(current_session, '.' + input_dir + file)
+    current_loaded_track_id = post_gpx(current_session, os.path.join(input_dir, file))
     print('track' + file + ' loaded')
     print('TrackId: ' + current_loaded_track_id)
     get_track_details(current_session, current_loaded_track_id)
